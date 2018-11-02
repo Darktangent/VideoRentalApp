846909ab5bd0c59e86990cc94262598775fd7d07
Rohan <c1ph3r9@gmail.com>
2018-11-02 12:14:03 -0500

    updating customer endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -49,6 +49,64 @@ router.post('/', async(req,res)=>{
 
 })
 
+router.put('/:id', async (req, res)=>{
+    const {error} = validateCustomer(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const customer= await Customer.findByIdAndUpdate(req.params.id, {
+        name:req.body.name,
+        isGold: req.body.isGold,
+        phone:req.body.phone
+    },{new:true})
+    if(!customer) return res.status(404).send('The customer with the provided id was not found')
+
+    res.send(customer)
+})
+
+
+router.delete('/:id', async (req,res)=>{
+
+    const customer= await Customer.findByIdAndDelete(req.params.id)
+    if(!customer) return res.status(404).send("The customer with that id was not found")
+    res.send(customer)
+
+})
+
+router.get('/id', async (req,res)=>{
+
+    const customer= await Customer.findById(req.params.id)
+    if(!customer) return res.status(404).send('Customer with that id was not found')
+    res.send(customer)
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function validateCustomer(customer) {
     const schema = {
       name: Joi.string().min(5).max(50).required(),
