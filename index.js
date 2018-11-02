1ef434de468022078e6088b1f2d91d94afad87cb
Rohan <c1ph3r9@gmail.com>
2018-11-02 10:23:01 -0500

    Adding customer route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+const customers=require('./routes/customers')
 
 mongoose.connect('mongodb://localhost/vidly-app')
   .then(() => console.log('Connected to MongoDB...'))
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/vidly-app')
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
